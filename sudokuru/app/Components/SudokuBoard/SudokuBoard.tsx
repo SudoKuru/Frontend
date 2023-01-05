f63251b07e49a845e95519ab187dd9dbc6b0c1b3
Arthur Upfield <arthurupfield@outlook.com>
2023-01-04 20:58:53 -0500

    Board Prototype, Input Sanitization Test

--- sudokuru/app/Components/SudokuBoard/SudokuBoard.tsx
+++ sudokuru/app/Components/SudokuBoard/SudokuBoard.tsx
@@ -1,14 +1,42 @@
 import * as React from 'react'
 import { Platform, View } from 'react-native'
 import './SudokuBoard.css'
+import { setStatusBarBackgroundColor } from 'expo-status-bar'
+
+const input = [
+    [1, 2, 3, 4, 5, 6, 7, 8, 9], 
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1], 
+    [1, 2, 3, 4, 5, 6, 7, 8, 9], 
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1], 
+    [1, 2, 3, 4, 5, 6, 7, 8, 9], 
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1], 
+    [1, 2, 3, 4, 5, 6, 7, 8, 9], 
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1], 
+    [1, 2, 3, 4, 5, 6, 7, 8, 9],
+]
 
 const SudokuBoard = () => {
 
+    const [inputArray, setInput] = React.useState(getDeepCopy(input));
+
+    function getDeepCopy(arr: number[][]) {
+        return JSON.parse(JSON.stringify(arr));
+    }
+
+    function sanitizeCell (e: React.ChangeEvent<HTMLInputElement>, row: number, col: number) {
+        var userInput = parseInt(e.target.value) || -1, grid = getDeepCopy(inputArray);
+
+        if (userInput === -1 || userInput >= 1 && userInput <= 9) {
+            grid[row][col] = userInput;
+        }
+
+        setInput(grid);
+    }
+
     return (
         
-
         <div className='Sudokuru'>
-            <div className='Sudoku Mentor'>
+            <div>
 
                 <View style={{flex : 1}}>
                 {Platform.OS === 'ios' && (
@@ -30,10 +58,10 @@ const SudokuBoard = () => {
                     <tbody>
                         {
                             [0, 1, 2, 3, 4, 5, 6, 7, 8].map((row, rowIndex) => {
-                                return <tr key={rowIndex}>
+                                return <tr key={rowIndex} className={((row + 1) % 3 === 0) ? 'hSquare' : ''}>
                                     {[0, 1, 2, 3, 4, 5, 6, 7, 8].map((col, colIndex) => {
-                                        return <td key={rowIndex + colIndex}>
-                                            <input className='cell'/>
+                                        return <td key = {rowIndex + colIndex} className={(col % 3 === 0) ? 'vSquare' : ''}>
+                                            <input value = {inputArray[row][col] === -1 ? '' : inputArray[row][col]} onChange = {(e) => sanitizeCell(e, row, col)} className = 'cell'/>
                                         </td>
                                     })}
                                 </tr>
@@ -41,7 +69,6 @@ const SudokuBoard = () => {
                         }
                     </tbody>
                 </table>
-
             </div>
         </div>
         
