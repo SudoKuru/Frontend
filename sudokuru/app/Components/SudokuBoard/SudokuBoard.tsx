4cc85b052d60146b2a1f954f70db013ad20e0835
Arthur Upfield <arthurupfield@outlook.com>
2023-01-10 18:54:06 -0500

    Improved Board Look
    Working on highlighting, may want to take an easier approach with the table by defining it statically and not dynamically creating the cells with map. Editing CSS on the fly is tricky lol

--- sudokuru/app/Components/SudokuBoard/SudokuBoard.tsx
+++ sudokuru/app/Components/SudokuBoard/SudokuBoard.tsx
@@ -4,29 +4,29 @@ import './SudokuBoard.css'
 import { setStatusBarBackgroundColor } from 'expo-status-bar'
 
 const input = [
-    [1, 2, 3, 4, 5, 6, 7, 8, 9], 
-    [-1, -1, -1, -1, -1, -1, -1, -1, -1], 
-    [1, 2, 3, 4, 5, 6, 7, 8, 9], 
-    [-1, -1, -1, -1, -1, -1, -1, -1, -1], 
-    [1, 2, 3, 4, 5, 6, 7, 8, 9], 
-    [-1, -1, -1, -1, -1, -1, -1, -1, -1], 
-    [1, 2, 3, 4, 5, 6, 7, 8, 9], 
-    [-1, -1, -1, -1, -1, -1, -1, -1, -1], 
-    [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    [0, 7, 2, 1, 9, 3, 5, 6, 0], 
+    [9, 0, 0, 0, 0, 0, 3, 4, 2], 
+    [1, 3, 0, 0, 2, 0, 0, 0, 7], 
+    [0, 1, 7, 4, 3, 5, 0, 2, 6], 
+    [3, 0, 0, 2, 7, 0, 0, 0, 0], 
+    [2, 0, 5, 0, 0, 1, 4, 0, 0], 
+    [0, 0, 3, 7, 4, 0, 0, 8, 0], 
+    [7, 0, 0, 3, 0, 0, 2, 0, 9], 
+    [5, 0, 0, 0, 0, 2, 0, 0, 0],
 ]
 
 const SudokuBoard = () => {
 
-    const [inputArray, setInput] = React.useState(getDeepCopy(input));
+    const [inputArray, setInput] = React.useState(parseJSON(input));
 
-    function getDeepCopy(arr: number[][]) {
+    function parseJSON(arr: number[][]) {
         return JSON.parse(JSON.stringify(arr));
     }
 
     function sanitizeCell (e: React.ChangeEvent<HTMLInputElement>, row: number, col: number) {
-        var userInput = parseInt(e.target.value) || -1, grid = getDeepCopy(inputArray);
+        var userInput = parseInt(e.target.value) || 0, grid = parseJSON(inputArray);
 
-        if (userInput === -1 || userInput >= 1 && userInput <= 9) {
+        if (userInput === 0 || userInput >= 1 && userInput <= 9) {
             grid[row][col] = userInput;
         }
 
@@ -58,10 +58,14 @@ const SudokuBoard = () => {
                     <tbody>
                         {
                             [0, 1, 2, 3, 4, 5, 6, 7, 8].map((row, rowIndex) => {
-                                return <tr key={rowIndex} className={((row + 1) % 3 === 0) ? 'hSquare' : ''}>
+                                return <tr key = {rowIndex} className = {((row + 1) % 3 === 0) ? 'hSquare' : ''}>
                                     {[0, 1, 2, 3, 4, 5, 6, 7, 8].map((col, colIndex) => {
-                                        return <td key = {rowIndex + colIndex} className={(col % 3 === 0) ? 'vSquare' : ''}>
-                                            <input value = {inputArray[row][col] === -1 ? '' : inputArray[row][col]} onChange = {(e) => sanitizeCell(e, row, col)} className = 'cell'/>
+                                        return <td key = {rowIndex + colIndex} className = {(col % 3 === 0) ? 'vSquare' : ''}>
+                                            <input 
+                                            value = {inputArray[row][col] === 0 ? '' : inputArray[row][col]} 
+                                            onChange = {(e) => sanitizeCell(e, row, col)} 
+                                            className = 'cell' 
+                                            readOnly = {input[row][col] != 0}/>
                                         </td>
                                     })}
                                 </tr>
@@ -75,6 +79,5 @@ const SudokuBoard = () => {
     );
 
 }
-
 
 export default SudokuBoard;
